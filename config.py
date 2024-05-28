d35b69117b6114fc78d4c54d1ec593028ab00b2c
p2p-sys <37340795+p2p-sys@users.noreply.github.com>
2024-05-28 19:17:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,20 @@ images_dir = '/path/to/snapshots'
 
 results_dir = '/path/to/results'
 
+models_dir = 'models'
+
+# Model URLs
+model_coco_url = "https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5x6.pt"
+model_365_url = "https://github.com/ultralytics/yolov5/releases/download/v6.0/yolov5m_Objects365.pt"
+
+# Model filenames
+model_coco_filename = "yolov5x6.pt"
+model_365_filename = "yolov5m_Objects365.pt"
+
 debug = False
 
 objects_labels = False
 
 class_labels = True
+
+delete_processed_file = False
